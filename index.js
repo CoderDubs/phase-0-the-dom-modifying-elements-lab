8050150ed81627abb79bfa03458628b852bb7df6
+ <+>
2023-12-21 12:10:36 -0800

    This could get complicated, slightly nervous here. Trees can be given many new leafs.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+var mainNode = document.getElementById("main");
+if (mainNode) {
+    mainNode.remove();
+}
+
+var newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+var yoName = 'Coder Dubs'
+newHeader.textContent = yoName + ' is the champion'
